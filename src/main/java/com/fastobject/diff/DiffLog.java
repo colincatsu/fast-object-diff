ba1456a65abc60e0e59655b1ff2c3a0f4ad30301
colinsu <colinsu@colinsudeMacBook-Pro.local>
2022-02-10 22:47:17 +0800

    修改pom文件

--- src/main/java/com/fastobject/diff/DiffLog.java
+++ src/main/java/com/fastobject/diff/DiffLog.java
@@ -5,22 +5,26 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * Created by colinsu
+ * @author colinsu
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DiffLog {
     /**
      * 汉字全称
+     * @return name
      */
     String name();
 
     /**
      * Date 如何格式化，默认可以为空
+     * @return dateFormat
      */
     String dateFormat() default "";
 
     /**
      * 是否忽略该值
+     * @return ignore
      */
     boolean ignore() default false;
 
